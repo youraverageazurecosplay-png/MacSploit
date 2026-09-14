7bec42f2ab18453459341c4157d7fe1908d5e295
Matthew Yip <youraverageazurecosplay@gmail.com>
2026-09-15 01:52:29 +0800

    Refactor Roblox.sh for architecture detection
    Refactor main function to improve readability and maintainability. Added architecture detection and Rosetta installation prompt.

--- Roblox.sh
+++ Roblox.sh
@@ -1,41 +1,13 @@
 #!/bin/bash
 
-main() {
-    clear
-    local architecture=$(arch)
+local architecture=$(arch)
 
     if [ "$architecture" == "arm64" ]
     then
+        echo -e "Detected ARM64 Architecture."
         if [ ! -f /Library/Apple/usr/libexec/oah/libRosettaRuntime ]
-        then
+        then # install rosetta if not available
+            echo -e "Prompting Rosetta (Not Installed)"
             softwareupdate --install-rosetta --agree-to-license
         fi
     fi
-
-    local robloxVersionInfo=$(curl -s "https://roblox.com")
-    local version=$(echo "$robloxVersionInfo" | grep -o '"clientVersionUpload":"[^"]*' | cut -d'"' -f4)
-
-    if [ -z "$version" ]
-    then
-        exit 1
-    fi
-
-    [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
-    
-    if [ "$architecture" == "arm64" ]
-    then
-        curl -s "http://rbxcdn.com" -o "./RobloxPlayer.zip"
-    else
-        curl -s "http://rbxcdn.com" -o "./RobloxPlayer.zip"
-    fi
-    
-    [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
-
-    unzip -o -q "./RobloxPlayer.zip"
-    mv ./RobloxPlayer.app /Applications/Roblox.app
-    rm ./RobloxPlayer.zip
-    
-    exit 0
-}
-
-main
