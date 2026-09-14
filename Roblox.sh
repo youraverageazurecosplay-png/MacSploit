570071c2c0892c0e35ce727d00f06e1ecf06ad91
Matthew Yip <youraverageazurecosplay@gmail.com>
2026-09-15 01:54:06 +0800

    Refactor Roblox.sh for architecture handling and downloads

--- Roblox.sh
+++ Roblox.sh
@@ -1,13 +1,34 @@
-#!/bin/bash
-
-local architecture=$(arch)
-
-    if [ "$architecture" == "arm64" ]
+echo -e "Downloading Latest Roblox..."
+    [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
+    local robloxVersionInfo=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer")
+    local versionInfo=$(curl -s "https://api.macsploit.dev/main/version.json")
+    
+    local mChannel=$(echo $versionInfo | ./jq -r ".channel")
+    local version=$(echo $versionInfo | ./jq -r ".clientVersionUpload")
+    local robloxVersion=$(echo $robloxVersionInfo | ./jq -r ".clientVersionUpload")
+    
+    if [ "$architecture" == "arm64" ] && [ "$mChannel" != "intel" ]
     then
-        echo -e "Detected ARM64 Architecture."
-        if [ ! -f /Library/Apple/usr/libexec/oah/libRosettaRuntime ]
-        then # install rosetta if not available
-            echo -e "Prompting Rosetta (Not Installed)"
-            softwareupdate --install-rosetta --agree-to-license
+        if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
+        then
+            curl "http://setup.rbxcdn.com/mac/arm64/$robloxVersion-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+        else
+            curl "http://setup.rbxcdn.com/mac/arm64/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+        fi
+    else
+        if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
+        then
+            curl "http://setup.rbxcdn.com/mac/$robloxVersion-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+        else
+            curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
         fi
     fi
+    
+    echo -n "Installing Latest Roblox... "
+    [ -d "./Applications/Roblox.app" ] && rm -rf "./Applications/Roblox.app"
+    [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
+
+    unzip -o -q "./RobloxPlayer.zip"
+    mv ./RobloxPlayer.app /Applications/Roblox.app
+    rm ./RobloxPlayer.zip
+    echo -e "Done."
